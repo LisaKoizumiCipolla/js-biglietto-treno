bf2c31893ec18f32b43644c0e58933ced7736c10
LisaKoizumiCipolla <cipollalisa17@gmail.com>
2023-05-12 15:30:01 +0200

    Add comment

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,14 @@
 
+//Retrieve user age
 const passengerAge = parseInt(prompt("Insert age of passenger"));
 
+//Retrieve travel distance
 const travelDistance = parseInt(prompt("Inset how much you want to travel (in km, barbarian!)"));
 
+//Starting price
 let price = (travelDistance * 0.21).toFixed(2);
 
+// Calc discount
 let discount 
 
 if ( passengerAge < 18) {
@@ -17,12 +21,12 @@ if ( passengerAge < 18) {
 
 console.log(price)
 
+//Calc final price
 let finalPrice = price - ((discount / 100) * price);
 
 console.log(finalPrice)
 
-
-
+//Insert results on html to show result to the user
 document.getElementById('passenger-age').innerHTML += passengerAge;
 
 document.getElementById('travel-distance').innerHTML += travelDistance;
